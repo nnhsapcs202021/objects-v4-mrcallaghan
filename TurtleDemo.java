dc91ffb478a401bfda72e8c6691c5d5d6a755b7b
Matt Callaghan <mcallaghan@naperville203.org>
2020-09-09 15:55:04 -0500

    recreated notes files from 9/9/2020 live coding for new bluej commit/push process

--- TurtleDemo.java
+++ TurtleDemo.java
@@ -1,13 +1,47 @@
+/**
+ * Every Java program contains one or more classes (e.g., HelloPrinter).
+ * 
+ * In general, every source file (e.g., HelloPrinter.java) contains one class (e.g., HelloPrinter).
+ * 
+ * The source file name must match the class name.
+ * 
+ * By convention, class names start with an uppercase letter.
+ *
+ * @author mcallaghan
+ * @version 09 September 2020
+ */
 import java.awt.Color;
 
 public class TurtleDemo
 {
+    
+    /*
+     * A class contains methods.
+     * 
+     * Most Java programs contain a class with a main method.
+     * 
+     * The main method is executed when the program starts.
+     */
     public static void main(String[] args)
     {
-        World ocean = new World();
-        Turtle crush = new Turtle(ocean);
-        crush.setPenColor(Color.ORANGE);
+        World ocean = new World();  // create a new World; a canvas on which Turtle objects can draw
+        Turtle crush = new Turtle(ocean); // create a new Turtle object from the Turtle class, referenced by the variable 'crush'
+        crush.setPenColor(Color.ORANGE);  // run methods on the turtle object
         crush.penDown();
         crush.forward(50);
+        
+        /*
+         * A method contains statements.
+         * 
+         * Statements may invoke other methods (e.g., forward).
+         * 
+         * In general, methods are invoked on objects (e.g., crush).
+         * 
+         * When invoking a method, arguments are passed in parentheses (e.g., 50).
+         */
+        crush.forward(50);    // move crush forward 50 pixels
+        
+        // this is a single-line comment (like # python)
+        
     }
 }
