ebafa4b90d3114b6a0d2fb4d38bee3eb271e9d79
Matt Callaghan <mcallaghan@naperville203.org>
2020-09-22 14:32:41 -0500

    finished StringExplorer notes

--- StringExplorer.java
+++ StringExplorer.java
@@ -2,13 +2,27 @@
 /**
  * Exploring the String class and its methods.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author mrcallaghan
+ * @version 22sep2020
  */
 public class StringExplorer
 {
     public static void main(String[] args)
     {
+        String river = new String("Mississippi");
+        String river2 = "Mississippi"; // string literal
+        river.replace("i", "x");
+        System.out.println(river);
         
+        /*
+         * The replace method returns a reference to a new String object.
+         *   It does not change the String object on which it is invoked.
+         * The String class has no mutator methods.  It is not possible to change a String object.
+         * 
+         * Strings are immutable.
+         */
+        
+        String riverX = river.replace("i", "x");
+        System.out.println(riverX);
     }
 }
--- StringExplorer7th.java
+++ StringExplorer7th.java
@@ -2,13 +2,28 @@
 /**
  * Exploring the String class and its methods.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author mrcallaghan
+ * @version 22sep2020
  */
 public class StringExplorer7th
 {
     public static void main(String[] args)
     {
+        String river = new String("Mississippi");
+        String river2 = "Mississippi"; // String literal
+        river.replace("i", "x");
+        System.out.println(river);
+        
+        /*
+         * The replace method returns a reference to a new String.
+         *   It does not change the String object on which it is invoked.
+         * The String class has no mutator methods.  It is not possible to change a String object.
+         *   Strings are immutable.
+         */
+        
+        String riverX = river.replace("i", "x");
+        System.out.println(riverX);
+        System.out.println(river);
         
     }
 }
